a98b6cb09c3c99afaa04f2666a7d33114de0bd1a
TTBob <erik.k.lagerstedt@gmail.com>
2025-01-20 11:36:23 +0100

    Add background music and volume setting

--- menu.gd
+++ menu.gd
@@ -6,6 +6,13 @@ extends Control
 
 @export var screen: int = 0
 
+func _ready():
+	var audio_player = AudioStreamPlayer.new()
+	audio_player.stream = load("res://audio/music_final_1.mp3")
+	audio_player.autoplay = true
+	audio_player.bus = "Background Music"
+	add_child(audio_player)
+
 func _process(_delta):
 	main.set_position(main.position.lerp(Vector2(get_viewport().get_visible_rect().size.x * screen, 0),0.2))
 	settings.set_position(settings.position.lerp(Vector2(get_viewport().get_visible_rect().size.x * (screen+1), 0),0.2))
